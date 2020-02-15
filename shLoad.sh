970078915470f562b8ba5b64954ded69d98296db
Viktor Bushmin <viktor.bushmin@gmail.com>
2020-02-16 00:53:45 +0300

    added ghostinspector.com test to submit order

--- shLoad.sh
+++ shLoad.sh
@@ -20,10 +20,11 @@ APIKEY=$(echo $DATA | $jq -r ".data")
 MONTHS=$(echo '6,7,8' | sed -e 's/,/%2c/g')
 BEGIN=2015-01-01
 END=2020-01-01
-COUNT=100
+COUNT=10
 
 DATA=$(curl 'https://earthexplorer.usgs.gov/inventory/json/v/1.4.0/search?jsonRequest=%7b%22apiKey%22:%22'$APIKEY'%22,%22datasetName%22:%22LANDSAT_8_C1%22,%22maxCloudCover%22:30,%22minCloudCover%22:0,%22temporalFilter%22:%7b%22startDate%22:%22'$BEGIN'%22,%22endDate%22:%22'$END'%22%7d,%22months%22:['$MONTHS'],%22maxResults%22:'$COUNT',%22additionalCriteria%22:%7b%22filterType%22:%22between%22,%22fieldId%22:25171,%22firstValue%22:19,%22secondValue%22:90%7d,%22spatialFilter%22:%7b%22filterType%22:%22mbr%22,%22lowerLeft%22:%7b%22latitude%22:53,%22longitude%22:0%7d,%22upperRight%22:%7b%22latitude%22:55,%22longitude%22:170%7d%7d,%22includeUnknownCloudCover%22:false%7d')
 RESULT=$(echo $DATA | $jq -j '.data.results[].entityId + " "')
-for ent in $RESULT; do curl -c login.cook -b login.cook -L 'https://earthexplorer.usgs.gov/order/addbulkscene?scenes='$ent'&collection_id=12864&originator=INVSVC' 2> /dev/null; done
+for ent in $RESULT; do curl -c login.cook -b login.cook -L 'https://earthexplorer.usgs.gov/order/addbulkscene?scenes='$ent'&collection_id=12864&originator=INVSVC' 2> /dev/null | jq -C; done
 DATA=$(curl 'https://earthexplorer.usgs.gov/inventory/json/v/1.4.0/logout?jsonRequest=%7b%22apiKey%22:%22'$APIKEY'%22%7d')
-echo 'DATA=' $DATA | jq -C
+echo 'DATA=' $DATA
+curl https://api.ghostinspector.com/v1/tests/5e4861bca86a49794d824fd5/execute/?apiKey=08be0ee1f51f733da0d300dbcead9bb7edaff265  | jq -C
